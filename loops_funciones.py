84026d3da72bbc354c0cdffbc73bbe4dc57efe6a
Hertzz729 <101437898+Hertzz729@users.noreply.github.com>
2023-04-04 10:00:19 -0600

    Actualización de loops_funciones.py

--- loops_funciones.py
+++ loops_funciones.py
@@ -104,7 +104,7 @@ def saludo():
     #------------------------------------
     # Documentacion rápida de la funcion
     #------------------------------------
-    '''Esta función saluda'''
+     '''Esta función saluda'''
     print("¡Quiuboles con los chavos! libro de Yordi Rosado")
 
 #========================
@@ -112,6 +112,146 @@ def saludo():
 #========================
 saludo()
 
-#================================
-# Se ejecuta pero no se asigna minuto 2:00
-#================================
+#==============================
+# Se ejecuta pero no se asigna
+#==============================
+salida = saludo()
+
+#===================
+# Esto no funciona
+#===================
+print(salida)
+
+#==========================
+# Mostrar Documentacion
+#==========================
+#help(saludo)
+
+#==========================
+# Funcion con argumento
+#==========================
+def salud2(nombre):
+    """"Esta funcion te saluda por tu nombre"""
+    print("!Que onda ", nombre,"¡")
+salud2("Jerson")
+salud2("Sana")
+
+#=================================
+# Ahorrar trabajo del interprete
+# nombre:str la varible es un str
+#=================================
+def saludos(nombre:str):
+    """Esta funcion te saluda por tu nombre estrictamente"""
+    print("!Que pasó ", nombre,"¡")
+    
+a=123
+print(type(a))
+saludos("Jerson")
+saludos(a)
+
+#=====================================
+# Funcion con muchos argumentos
+#=====================================
+def saludos_multiples(nombre1, nombre2, nombre3):
+    """Esta funcón salida a 3 personas la mismo tiempo"""
+    print("Hola", nombre1, nombre2, "y", nombre3)
+    
+saludos_multiples("Jerson", "Marcus", "Eren")
+
+#=============================================
+# Función con cualquier número de argumentos
+#=============================================
+def muchos_saludos(*nombres):
+    """Esta función saluda a todos los que quieras"""
+    i=0
+    #=================================
+    # end= es para ponerlo de corrido
+    #=================================
+    print("Hola ", end="")
+    while len(nombres)>i:
+        # Ultimo nombre
+        if (i==len(nombres)-1):
+            print(nombres[i])
+        else:
+            # Cualquier otro nombre
+            print(nombres[i], end=", ")
+        i+=1
+        
+muchos_saludos("Jerson", "Sana", "Tanjiro", "Eren")
+
+
+#===========================================
+# Función con argumentos escondidos **
+#===========================================
+
+def greet(**person):
+    #=====================================================
+    # persona tiene características firstname y lastname
+    #=====================================================
+    print("Hello", person["firstname"], person["lastname"])
+
+#==============================================
+# Llamar la función con argumentos en desorden
+#==============================================
+greet(lastname="Jobs", firstname="Steve") # Se pueden especificar las variables en desorden
+greet(firstname="Calvin", lastname="Harris")
+greet(lastname="Ronaldo", firstname="Cristiano")
+greet(firstname="Bill", lastname="Gates", age=55) # se  pueden pasar mas pramentross de los necesarios
+
+#=====================================
+# Función con los valores por defecto
+#=====================================
+def greet(name="Guest"):
+    print("Hello", name)
+
+greet() # Utiliza el valor por defecto
+greet("Cronos")
+
+#=======================
+# Función con resultado
+#=======================
+def suma(a,b):
+    return a+b
+
+#=========================
+# Programación funcional 
+#=========================
+total=suma(4, suma(4,230))
+print(total)
+ 
+#==================================================
+# Calculo lambda
+# nombre de la función = lambda variable : función
+#==================================================
+x_al_cuadrado = lambda x: x*x
+al = x_al_cuadrado(5)
+print(al)
+
+#===============================
+# Lambda de varias variables
+#===============================
+suma = lambda x1,x2,x3 : x1+x2+x3
+print(suma(32,5,364))
+
+sumas = lambda *x: x[0] +x[1] + x[2] +x[3]
+print(sumas(100,200,300,400))
+
+#===========================================
+# Uso de una función anónima 
+# El argumento va afuera entre párentesis
+#===========================================
+
+print((lambda x: x*x)(6)) #función anónima
+
+#==================================
+# Función ocn variable global
+# EVITE EL EXCESO!!!!!!
+#==================================
+name="Steve"
+def greet():
+    global name # Para utilizar una variable global (que viene de fuera del bloque)
+    name="Hertzz"
+    print("Hello", name)
+    
+greet()
+
