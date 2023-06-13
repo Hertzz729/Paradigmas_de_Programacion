0d2a770abda2055684deaed8e7a95652752216e2
Hertzz729 <101437898+Hertzz729@users.noreply.github.com>
2023-06-13 15:59:02 -0600

    Update calor2d.py

--- calor2d.py
+++ calor2d.py
@@ -1,4 +1,4 @@
-import numoy as np
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -17,7 +17,7 @@ pasos =100
 
 # Tamaño de las celdas
 dx = L/n
-udx = 1.0/(dx*dx)
+udx2 = 1.0/(dx*dx)
 # Paso de tiempo
 dt = 0.25*(min(dx[0], dx[1])**2)/k
 print("dt = ", dt)
@@ -30,5 +30,30 @@ un = np.zeros(nt,dtype=np.float64) # arreglo de escritura
 
 def evolucion(u,n,udx2,dtm,i,k):
     jpl = i +n[0]
-    jml = i
+    jml = i -n[0]
+    laplaciano = (u[i-1]-2.0*u[i]+u[i+1])*udx2[0]+(u[jml]-2.0*u[i]+u[jpl])*udx2[1]
+    unueva = u[i] +dt*k*laplaciano
+    return unueva
+    
+def solucion(u,un, udx2,dt,n,k):
+    for jj in range(1,n[1]-1):
+        for ii in range(1,n[0]-1):
+            i= ii +n[0]*jj
+            unueva = evolucion(u,n,udx2,dt,i,k)
+            if 9 == int(nt/2)+int(n[0]/2):
+                unueva = 1.0
+            un[i]= unueva
 
+start = time.time()
+for t in range(1,pasos+1):
+    solucion(u, un , udx2, dt, n, k)
+    u =un
+    if t%100==0: print("Iteracion = ",t)
+end = time.time()
+print("Tardo: ", end-start,"s")
+
+u = np.rashape(u,(n[0],n[1]))
+x,y = np.meshgrid(np.arange(0,L[0],dx[0]), np.arange(0,L[1],dx[1]))
+ax = plt.axes(projection= "3d")
+ax.plot_surface(x,y,u,cmap=cm.hsv)
+plt.show()
